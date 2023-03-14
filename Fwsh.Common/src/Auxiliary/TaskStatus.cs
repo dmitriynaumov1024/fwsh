565397d507309e7265f915c0e33ca7c26aef7899
Dmitriy Naumov <naumov1024@gmail.com>
2023-03-14 09:31:32 +0200

    common: create TaskStatus

---
+++ Fwsh.Common/src/Auxiliary/TaskStatus.cs
@@ -0,0 +1,12 @@
+namespace Fwsh.Common;
+
+public static class TaskStatus
+{
+    public static readonly string
+        Unknown = "unknown",
+        Assigned = "assigned",
+        Rejected = "rejected",
+        Working = "working",
+        Finished = "finished",
+        Impossible = "impossible";
+}
