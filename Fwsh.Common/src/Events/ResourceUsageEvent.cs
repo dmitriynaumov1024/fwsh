92720bc21aa3930bf024e2d9af731dae908a1a1d
Dmitriy Naumov <naumov1024@gmail.com>
2023-03-14 09:22:55 +0200

    common: refactor resource usage event

--- Fwsh.Common/src/Events/ResourceUsageEvent.cs
+++ Fwsh.Common/src/Events/ResourceUsageEvent.cs
@@ -2,8 +2,13 @@ namespace Fwsh.Common;
 
 public class ResourceUsageEvent : BasicEvent
 {
-    public string ResourceType { get; set; }
     public int WorkerId { get; set; }
+
+    public string TaskType { get; set; }
+    public int TaskId { get; set; }
+
+    public string ResourceType { get; set; }
     public int ItemId { get; set; }
+
     public int Quantity { get; set; }
 }
