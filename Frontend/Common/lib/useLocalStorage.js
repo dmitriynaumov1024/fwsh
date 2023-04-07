8f7b839161afdc86b24b539ae6e0bb604333c137
Dmitriy Naumov <naumov1024@gmail.com>
2023-04-07 13:24:26 +0300

    Frontend: Common: add volatiles and 'created()' callback option

--- Frontend/Common/lib/useLocalStorage.js
+++ Frontend/Common/lib/useLocalStorage.js
@@ -9,18 +9,26 @@ function getStorageObject (storageId) {
     }
 }
 
-function createLocalStorage (storageId, initialValue, wrap = null) {
-    let storageObject = getStorageObject(storageId) ?? initialValue
-    if (wrap instanceof Function) storageObject = wrap(storageObject) 
+function createLocalStorage (storageId, { initial, wrap, volatile, created } = options) {
+    let storageObject = getStorageObject(storageId) ?? initial
+    if (wrap instanceof Function) {
+        storageObject = wrap(storageObject)
+    } 
+    if (created instanceof Function) {
+        created.call(storageObject)
+    }
     window.addEventListener("unload", () => {
+        if (volatile instanceof Array) {
+            for (const key of volatile) storageObject[key] = undefined 
+        }
         window.localStorage[storageId] = JSON.stringify(storageObject)
     })
     return storageObject
 } 
 
-function useLocalStorage (storageId, initialValue, wrap = null) {
+function useLocalStorage (storageId, options) {
     if (existingStorages[storageId] == undefined) {
-        existingStorages[storageId] = createLocalStorage(storageId, initialValue, wrap)
+        existingStorages[storageId] = createLocalStorage(storageId, options)
     }
     return existingStorages[storageId]
 }
