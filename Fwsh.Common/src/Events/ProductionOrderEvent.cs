3dff9b6591b9596c55828d546bafac127872ea07
Dmitriy Naumov <naumov1024@gmail.com>
2023-04-13 12:04:14 +0300

    Common: Events: add useful constructors

--- Fwsh.Common/src/Events/ProductionOrderEvent.cs
+++ Fwsh.Common/src/Events/ProductionOrderEvent.cs
@@ -8,4 +8,14 @@ public class ProductionOrderEvent : BasicEvent
     public int FabricColorId { get; set; }
     public int FabricTypeId { get; set; }
     public int Quantity { get; set; }
+
+    public ProductionOrderEvent(ProductionOrder order) : base()
+    {
+        this.OrderId = order.Id;
+        this.DesignId = order.DesignId;
+        this.FabricId = order.FabricId;
+        this.FabricColorId = order.Fabric.ColorId;
+        this.FabricTypeId = order.Fabric.FabricTypeId;
+        this.Quantity = order.Quantity;
+    }
 }
--- Fwsh.Common/src/Events/RepairOrderEvent.cs
+++ Fwsh.Common/src/Events/RepairOrderEvent.cs
@@ -3,4 +3,9 @@ namespace Fwsh.Common;
 public class RepairOrderEvent : BasicEvent
 {
     public int OrderId { get; set; }
+
+    public RepairOrderEvent (RepairOrder order): base()
+    {
+        this.OrderId = order.Id;
+    }
 }
