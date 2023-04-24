1fd891ff01d79eb9f3fbba4089ca7d538d8f5687
Dmitriy Naumov <naumov1024@gmail.com>
2023-04-24 16:54:41 +0300

    Frontend: Common: minor refactoring

--- Frontend/Common/lib/utils.js
+++
@@ -1,7 +0,0 @@
-
-export function arrayToDict (array) {
-    let result = { }
-    if (array.constructor == Array) 
-        for (const key of array) result[key] = true
-    return result 
-}
--- Frontend/Common/utils/index.js
+++ Frontend/Common/utils/index.js
@@ -1,4 +1,19 @@
-// side-effect
-console.log("Hell to world from @common/utils/index.js")
+// convert something like ["key1", "key2"] 
+// to { "key1": true, "key2": true }
+//
+function arrayToDict (array) {
+    let result = { }
+    if (array.constructor == Array) 
+        for (const key of array) result[key] = true
+    return result 
+}
 
-export default { }
+function cdnResolve (url) {
+    if (url?.startsWith && !url.startsWith("/")) url = "/" + url 
+    return import.meta.env.VITE_CDN_BASEURL + "/files" + url
+}
+
+export { 
+    arrayToDict,
+    cdnResolve,
+}
